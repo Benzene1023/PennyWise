9cebe00d0a2a7dc17f2032d4f0f79f6b19ff005a
Gourish Mondal <139765793+Benzene1023@users.noreply.github.com>
2024-09-04 19:10:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -31,9 +31,6 @@ app.options('*', cors({
 }));
 
 // Routes
-app.get("/",(req,res)=>{
-res.send("Hare Krishna");
-});
 app.use("/api/v1/transections", transectionRoute);
 app.use("/api/v1/users", userRoute);
 
